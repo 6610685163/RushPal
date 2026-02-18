a739efad7edc9d869d16bef391c0981dbec5dc1f
Kantapong Witchukraingkrai <6610625045@cn102>
2026-02-18 19:30:53 +0700

    demo backend

--- rushpal_app/lib/main.dart
+++ rushpal_app/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:rushpal/theme/app_theme.dart';
 import 'screens/login_screen.dart'; // import login screen
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(RushpalApp());
 }
 
